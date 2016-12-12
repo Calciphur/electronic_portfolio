b6f322f0bada5f9e80abe917940b08dde506f2ed
calciphur <cdcravens16@gmail.com>
2016-12-12 16:42:23 -0500

    Moved styles to separate css file. Added js contact window.

---
+++ scripts/controllers.js
@@ -0,0 +1,13 @@
+function openContactInfo() {
+
+    var contactInfoWindow = window.open("", "MsgWindow", "width=400, height=300");
+    contactInfoWindow.document.write("<p> Email Address: cdcravens16@gmail.com </p>");
+    
+}
+
+
+
+                                     
+                        
+                                     
+                                     
